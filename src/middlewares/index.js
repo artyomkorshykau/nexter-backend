2d13ac087d8d97c9b490e8e28dff8d35701cac7e
artyomkorshykau <butovichjunior@gmail.com>
2024-12-18 13:26:42 +0300

    add domains for cors

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -6,25 +6,7 @@ import documentSwagger from '../../src/docs/swagger.js'
 import swaggerUi from 'swagger-ui-express'
 
 export const applyMiddlewares = app => {
-  app.use((req, res, next) => {
-    res.setHeader(
-      'Content-Security-Policy',
-      "default-src 'self'; frame-src 'self' https://vercel.live;",
-    )
-    next()
-  })
-
-  app.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          defaultSrc: ["'self'"],
-          scriptSrc: ["'self'", "'unsafe-inline'", 'https://vercel.live'],
-          connectSrc: ["'self'", 'https://vercel.live'],
-        },
-      },
-    }),
-  )
+  app.use(helmet())
   app.use(express.json())
   app.use(cookieParser())
   app.use(
